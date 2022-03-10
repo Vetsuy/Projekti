b599b9c52d36db5a3af9946c4b37bf85547c4078
JoakimPekola <joakim.pekola@edu.keuda.fi>
2022-03-10 18:26:11 +0200

    lisäsin kommenteja ja tein rekisteröinnistä paremman
    jos rekisteröinissä ei laita salasanaa tulee viesti siitä

--- js/Kirjaudu.js
+++ js/Kirjaudu.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", kirjautunut)
 
-function kirjaudu_ulos(){
+function kirjaudu_ulos(){// muuttaa kaikki arvot local storagesta arvoon jota ei käytetä kun painaa kirjaudu ulos nappia
     localStorage.setItem("rek","ei");
     localStorage.setItem("arvo", "ei");
     localStorage.setItem("kirjautunut","ei")
@@ -8,32 +8,37 @@ function kirjaudu_ulos(){
     localStorage.setItem("valmis","e")
     localStorage.setItem("vaa","e")
 }
-function rekisteröidy2(){
+function rekisteröidy2(){// pyytää nimeä ja salasanaa. jos nimi on jo local storagessa paljastaa viestin joka kertoo että nimi on varattu. jos nimi ei ole varattu tallentaa nimen ja salasanan local storageen. jos ei laitanut salasanaa näyttää tekstiä
     let as = Object.keys(localStorage)
+    let a = document.getElementById("salasa").value
     for (let i = 0;i<as.length;i++){
         let ggg =  localStorage.key(i)
         if(document.getElementById("nimi1").value === ggg){
                 localStorage.setItem("errr","r")
                 break;
-        }if(i+1===as.length){
+        }
+        if(a===""){
+            localStorage.setItem("errr","l")
+        }
+        else if(i+1===as.length){
             localStorage.setItem(document.getElementById("nimi1").value,document.getElementById("salasa").value)
             localStorage.setItem("valmis","k")
-            localStorage.setItem("err","e")
+            localStorage.setItem("errr","e")
             localStorage.setItem("text",document.getElementById("nimi1").value)
             break;
-        }
+        
+        } 
         
     }
-    localStorage.setItem(i,"as")
+    
 }
-function rekisteröidy(){
+function rekisteröidy(){//vaihtaa arvon local storagesta kun painaa ekaa rekisteröidy nappia
     localStorage.setItem("rek","kylla")
 }
-function kirjaudu(){
-    document.getElementById("Kirjautumis_nappi").style.display="none"
+function kirjaudu(){//vaihtaa arvon local storagesta kun painaa ekaa kirjaudu nappia
     localStorage.setItem("arvo", "k")
 }
-function taka(){
+function taka(){//muuttaa kaikki arvot local storagesta arvoon jota ei käytetä kun painaa takaisin nappia
     localStorage.setItem("rek","ei");
     localStorage.setItem("arvo", "ei");
     localStorage.setItem("kirjautunut","ei")
@@ -41,7 +46,7 @@ function taka(){
     localStorage.setItem("valmis","e")
     localStorage.setItem("vaa","e")
 }
-function kirja(){
+function kirja(){//etsii katsoo onko annettu nimi ja salasana local sotaregessa jos kumpikaan on väärin paljaastaa tekstin joka kertoo että nimi tai salasana on väärin
     let as = Object.keys(localStorage)
     let ni = document.getElementById("nimi").value;
     for (let i = 0;i<as.length;i++){
@@ -57,28 +62,31 @@ function kirja(){
     }
 }
 
-function kirjautunut(){//funktio alkaa kun sivu ladataan 
+function kirjautunut(){//funktio alkaa kun sivu ladataan ja aluksi piilottaa kaiken mitä ei tarvitse viellä nähdä
+    document.getElementById("eros").style.display="none"
     document.getElementById("vaara").style.display="none"
     document.getElementById("ero").style.display="none";
     document.getElementById("kirjaudu_ulos").style.display="none";
     document.getElementById("rekisteri").style.display="none"
     document.getElementById("kirjaudu_lomake").style.display="none";
-    if (localStorage.getItem("arvo")=== 'k'){
+    if (localStorage.getItem("arvo")=== 'k'){//jos painaa ensimmäistä kirjautumis nappia piilottaa rekisteri napin ja ekan kirjaudu napin sitten paljastaa kirjautumis lomakkeen
         document.getElementById("reikisteröidy").style.display="none"
     document.getElementById("Kirjautumis_nappi").style.display="none"
     document.getElementById("kirjaudu_lomake").style.display="block";
-    if(localStorage.getItem("vaa")=== 'kylla'){
+    if(localStorage.getItem("vaa")=== 'kylla'){//jos laittoi väärän salasanan tai nimen
         document.getElementById("vaara").style.display="block"
     }
     }
-    if(localStorage.getItem("rek")=== 'kylla'){
+    if(localStorage.getItem("rek")=== 'kylla'){//jos painoi ekaa rekisteri nappia piilottaa kirjaudu napin ja ekan reksiteri napin sitten näyttää rekisteri lomakkeen
         document.getElementById("rekisteri").style.display="block"
         document.getElementById("reikisteröidy").style.display="none"
         document.getElementById("Kirjautumis_nappi").style.display="none"
-        if(localStorage.getItem("errr")=== 'r'){
+        if(localStorage.getItem("errr")=== 'r'){//jos nimi on jo rekisterissä avaa tekstin
             document.getElementById("ero").style.display="block";
+        }else if(localStorage.getItem("errr")=== 'l'){//näyttää jos ei laitanut salasanaa
+            document.getElementById("eros").style.display="block";
         }
-        if(localStorage.getItem("valmis")=== 'k'){
+        if(localStorage.getItem("valmis")=== 'k'){//kun on rekisteröitynyt oikein antaa käyttäjä nimen näkyviin ja kirjaudu ulosnapin piillottaa kaiken muun
             document.getElementById("reikisteröidy").style.display="none"
     document.getElementById("Kirjautumis_nappi").style.display="none"
     document.getElementById("kirjaudu_ulos").style.display="block";
@@ -87,15 +95,7 @@ function kirjautunut(){//funktio alkaa kun sivu ladataan
             document.getElementById("nn").textContent += localStorage.getItem("text")
         }
     }
-    if (localStorage.getItem("rek")=== 'tehty'){
-        document.getElementById("reikisteröidy").style.display="none"
-    document.getElementById("Kirjautumis_nappi").style.display="none"
-    document.getElementById("kirjaudu_ulos").style.display="block";
-    document.getElementById("rekisteri").style.display="none"
-    document.getElementById("kirjaudu_lomake").style.display="none";
-    document.getElementById("käyttäjä_nimi").textContent += localStorage.getItem()
-    }
-    if(localStorage.getItem("kirjautunut")==='kylla'){// piillottaa kirjautumis lommakeen ja näyttää kirjaudu ulos napin
+    if(localStorage.getItem("kirjautunut")==='kylla'){// piillottaa kirjautumis lommakeen, näyttää kirjaudu ulos napin ja käyttäjä nimen 
         document.getElementById("reikisteröidy").style.display="none"
     document.getElementById("Kirjautumis_nappi").style.display="none"
     document.getElementById("kirjaudu_ulos").style.display="block";
